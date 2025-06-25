3e831f239c131ada48c92af2c4c67bc0b83d0358
Jose Ortiz Costa <joseortizcostadev@gmail.com>
2025-06-25 16:39:13 -0700

    Create permissions.sql

---
+++ SQL/permissions.sql
@@ -0,0 +1,42 @@
+/*
+ This file will implement databases business requirements for the topic of SQL permissions
+ */
+
+
+/*
+Business Requirement #1: Enable authorized employees in the
+                         sales department to increase the price of tracks within a specified
+                         genre (e.g., Jazz) by a given percentage. Note that this mechanism should
+                         only be accessible to sales department employees who have granted permission
+                         to access the Invoice table.
+
+
+*/
+
+
+/*
+ Business Requirement #2: Prevent a customer from being deleted if they have existing
+                          sales in the Sales table.
+
+                        Assumptions:
+                        (1) If a customer has sales records in the Sales table, they should not be allowed to be deleted.
+                        (2) The deletion attempt should be blocked with an error message to maintain data integrity.
+
+
+ */
+
+/*
+Business Requirement #3: Automatically archive old customer data that hasn't made a purchase in the last two years.
+
+                          Assumptions:
+                         (1) The system should identify customers who have not made any purchases in the last two years.
+                         (2) The data for these customers (including records in the `Customer` and `Invoice` tables)
+                             should be moved to an `ArchivedCustomerData` table for historical purposes.
+                         (3) The event should run once a month to ensure that inactive customer data is archived regularly.
+                         (4) In the event that one or more customers who have been archived make a purchase after being archived,
+                             their data should be returned to the original `Customer` and `Invoice` tables with their original customer ID.
+                        (5) The process of reactivating archived customers should ensure their records are properly
+                            re-associated with any new transactions.
+
+
+*/
