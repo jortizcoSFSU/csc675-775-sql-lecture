78bd68880c33bd506b37e9e3c5eb34f6293ef576
Jose Ortiz Costa <joseortizcostadev@gmail.com>
2025-10-20 09:02:09 -0700

    Organized comments in select.sql to make descriptions of the problems clear.

--- SQL/select.sql
+++ SQL/select.sql
@@ -38,255 +38,157 @@ to be retrieved. But not how to retrieve it. The HOW to retrieve the data is alr
 
 /* =============================== PRACTICE PROBLEMS =================================================================*/
 
+-- The following set of problems are specifically designed to learn every of the concepts mentioned above.
 
-/*
-    Retrieve all tracks from the database.
-
-
- */
+/* ===============  Problem 1 (Basic Query) ================
+   Problem:
+      Retrieve all tracks from the database.
+*/
     
 
 
+/* ===============  Problem 2 (Joining Tables) ================
+    Problem:
+       Find all tracks on the album "Objection Overruled".
 
-
-
-
-
-
-
-/*
-   
-    Find all tracks on the album "Objection Overruled".
-
-*/
-
--- (1) Using WHERE with fully qualified references
-
-
--- (2) Using INNER JOIN with alias-qualified references
-
-
-
-/*
-    Find all tracks in the genres "Metal" and 'Alternative & Punk' but exclude "Jazz".
-    Display the track title, album title, and genre description.
-
-    
+    Solve the problem using the two following approaches:
+       (1) Using WHERE with fully qualified references
+       (2) Using INNER JOIN with alias-qualified references
 */
 
 
 
-/*
-    -- Find all customers who purchased the track "Bury a Friend".
-    Display the customer name and the invoice number for that track.
-
-    --  How can we optimize this query in order for it to do less work?
-
+/* ===============  Problem 3 (Conditional Statement) ================
+    Problem:
+       Find all tracks in the genres "Metal" and 'Alternative & Punk' but exclude "Jazz".
+       Display the track title, album title, and genre description.
 
 */
 
 
 
+/* ===============  Problem 4 (Optimizations) ================
+    Problem:
+       Find all customers who purchased the track "Bury a Friend".
+       Display the customer name and the invoice number for that track.
 
-
-/*
-    Retrieve all tracks released between years 1990 and 1995 (both inclusive).
-    Order results by release year in descending order.
-    Display the artist name, track title, and release date.
-*/
-
--- (1) without column alias
-
-
-
-
-
--- (2) with column alias
-
-
-
-
--- (3) Show only the most recent released track
-
-
-
--- (4) Show the oldest released track
-
-
-
-/*
-    Solve the following problem using four different approaches:
-       - Multiple Computations for the same value
-       - A Common Table Expression (CTE)
-       - A View
-       - A Subquery
-
-    For each track, categorize its length as "Too Long," "Normal," or "Too Short."
-    Display the track title, artist name, duration, and the category.
-
-    If the track length >= 6 minutes then categorize it as "Too long"
-    If the track length < 6 and >= 4 minutes then categorize it as "Normal"
-    If the track length <4 then categorize it as "Too Short"
+    Challenge:
+       Create an optimization for this problem where you only use one table instead of two.
 
 */
 
--- (1) Brute Forcing Repeated Computations
+
+/* ===============  Problem 5 (Ordering and Limiting) ================
+    Problem:
+       Retrieve all tracks released between years 1990 and 1995 (both inclusive).
+       Order results by release year in descending order.
+       Display the artist name, track title, and release date.
+
+    Solve this problem using the following four approaches:
+       (1) without column alias
+       (2) with column alias
+       (3) Show only the most recent released track
+       (4) Show the oldest released track
+
+
+*/
 
 
 
 
+/* ===============  Problem 6 (Cases, CTEs, VIEWs, and Subqueries) ================
+   Problem:
+       For each track, categorize its length as "Too Long," "Normal," or "Too Short."
+       such as:
+           -- If the track length >= 6 minutes then categorize it as "Too long"
+           -- If the track length < 6 and >= 4 minutes then categorize it as "Normal"
+           -- If the track length <4 then categorize it as "Too Short"
+       Display the track title, artist name, duration, and the category.
 
--- (2) Optimizing with a Common Table Expression. (CTE)
-   
+   Solve the following problem using four different approaches:
+       (1) Brute forcing
+       (2) A Common Table Expression (CTE)
+       (3) A View
+       (4) A Subquery
+*/
 
-
-
--- (3) Optimizing with a View
-
-
-
-
-
--- (4) Another version of this problem with a Subquery
-    
-
-
-
-
-
-/*
-    Find the total track sales per state where total sales exceed $5.
-    Order the results by total sales in descending order.
-    Display the state and the corresponding total sales.
+/* ===============  Problem 7 (GROUP BY and HAVING)================
+    Problem:
+        Find the total track sales per state where total sales exceed $5.
+        Order the results by total sales in descending order.
+        Display the state and the corresponding total sales.
 */
   
-
-
-
-
-/*
-    Find all tracks in each album where the track title contains 'o' at index position 1.
-    Display the album title and track title.
+/* ===============  Problem 8 (MATCHING)================
+    Problem:
+        Find all tracks in each album where the track title contains 'o' at index position 1.
+        Display the album title and track title.
 */
     
 
-/*
-    Find the number of tracks per album where the track title length exceeds 9 characters.
-    Display the album title and the count of such tracks.
+/* ===============  Problem 9 (LENGTH)================
+    Problem:
+        Find the number of tracks per album where the track title length exceeds 9 characters.
+        Display the album title and the count of such tracks.
 */
 
    
-
-
-
-/*
-    Retrieve all customers and their invoices, including customers without invoices.
-*/
-
- 
-
-
-
-/*
-    Retrieve all invoices and their associated customers, including invoices without customers.
+/* ===============  Problem 10 (JOINS TYPE) ================
+    Solve the following problems:
+       (1) Retrieve all invoices that have customers, and all customers who have invoices.
+       (2) Retrieve all customers and their invoices, including customers without invoices.
+       (3) Retrieve all invoices and their associated customers, including invoices without customers.
+       (4) Retrieve all customers who have no invoices.
+       (5) Retrieve all invoices that have no customers.
+       (6) Retrieve all invoices and all customers.
+       (7) Retrieve all invoices without customers and customers without invoices.
 */
 
 
+/* ===============  Problem 11 (Challenge Problem) ================
+    Problem:
+        Find all customers who purchased the same track. Display the customer name and track title.
 
-/*
-    Retrieve all invoices that have customers, and all customers who have invoices.
+    Solve the problem using the following approaches:
+        (1) Find an easy instance of the problem, and solve it.
+        (2) Once you understand the problem - after solving part (1) - solve the original problem
+
+    Challenge:
+        Solve the problem using a subquery.
+
 */
 
 
-
-/*
-    Retrieve all customers who have no invoices.
-*/
-
-
-
-
-/*
-    Retrieve all invoices that have no customers.
-*/
-
-
-/*
-    Retrieve all invoices and all customers.
-*/
-
-
-
-/*
-    Retrieve all invoices without customers and customers without invoices.
+/* ===============  Problem 12 (Recursive CTEs) ================
+   Problem:
+       Find the full referral chain for a given customer.
+       For example, Customer 1 -> referred 2 -> referred 3 -> referred 4
 */
 
 
 
 
-/*
-    Find all customers who purchased the same track.(instead of the same track, customer that purchased the track 'Bury a Friend')
-    Display the customer name and track title.
+
+
+/* ===============  Problem 13 (Window Functions) ================
+    Problem:
+        Find the second most popular track based on total sales (most popular = highest sales).
+        Display the track title and its total sales. Ensure you account for potential ties in the sales data.
+
+    Solve the problem using the following approaches:
+        (1) Using LIMIT
+        (2) Using a Common Table Expression (CTE)
+        (3) Using RANK function
+
+*/
 
     
-*/
-
--- (1) Easiest Instance of the problem: Find all customers who purchased track 'Bury a Friend'
-
-
-
-
-
-
--- (2) Solution for the hard problem
-
-
-
--- (3) Follow up: Solve the problem using a subquery
-
-
-
-/*
-   Find the full referral chain for a given customer (for example, Customer 1 -> referred 2 -> referred 3 -> referred 4)
-
-*/
-
-
-
-
-
-
-/*
-    Find the second most popular track based on total sales (most popular = highest sales).
-    Display the track title and its total sales.
-    Ensure you account for potential ties in the sales data.
-
-*/
-
--- (1) Why this problem can't be solved with a regular LIMIT or limit with OFFSET?
-
-
-
--- (2) Why this problem can't be solved with a regular Common Table Expression (CTE)
-
-
-
-
-
-
-
--- (3) Solve with a windows function: RANK()
-    
-
-
-
-
-/*
-    For each album, find the number of tracks where:
-    - The track title contains 'o' at index position 1.
-    - The average unit price of these tracks is greater than the overall average unit price of all invoices.
-
-    Display the album title, count of tracks, and their average unit price.
+/* ===============  Problem 14 (Free Style) ================
+   Testing your knowledge acquired from this lecture:
+        For each album, find the number of tracks where:
+            - The track title contains 'o' at index position 1.
+            - The average unit price of these tracks is greater than the overall average unit price of all invoices.
+        Display the album title, count of tracks, and their average unit price.
 */
 
 
